ac6522dd4baa34bb35af1c297e8b9964a54c5815
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2020-12-28 23:34:33 +0530

    added service worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react"
+import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
@@ -14,6 +15,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
